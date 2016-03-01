be914077e3644aaa24854f34ffdd52c08c7ca22c
Klim Kireev <edvmartell@gmail.com>
2016-03-01 10:45:49 +0300

    Add new function for porting sdcard driver

--- source/include/bcm2836/mmio.h
+++ source/include/bcm2836/mmio.h
@@ -1,14 +1,35 @@
 #ifndef __MMIO__
 #define __MMIO__
+#include "defines.h"
 
-static inline void mmio_write(uint32_t reg, uint32_t data)
+static inline void mmio_write(aptr_t addr, reg_t data)
 {
-	*(volatile uint32_t *)reg = data;
+	*((volatile uint32_t*) addr) = data;
 }
  
-static inline uint32_t mmio_read(uint32_t reg)
+static inline reg_t mmio_read(aptr_t addr)
 {
-	return *(volatile uint32_t *)reg;
+	return *(volatile reg_t*) addr;
 }
 
+static inline uint32_t mmio_dword_read(byte* addr)
+{
+        return (addr[0] | addr[1] << 8 | addr[2] << 16 | addr[3] << 24);
+}
+
+static inline void mmio_dword_write(byte* addr, reg_t data)
+{
+        addr[0] = data & 0xff;
+        addr[1] = (data & 0xff00) >> 8;
+        addr[2] = (data & 0xff0000) >> 16;
+        addr[3] = (data & 0xff000000) >> 24;
+
+        return;
+}
+
+static inline uint32_t byte_swap(uint32_t num)
+{
+        return (num & 0xff) << 24 | (num & 0xff00) << 8 |
+               (num & 0xff0000) >> 8 | (num & 0xff000000) >> 24;
+}
 #endif
